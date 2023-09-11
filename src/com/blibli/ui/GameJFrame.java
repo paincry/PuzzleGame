c0c950c8d3ac15631073cf87ee85d08f5f9dbfd4
Mr.pain <1477871185@qq.com>
2023-09-11 19:16:12 +0800

    基于java自带GUI的小游戏

--- src/com/blibli/ui/GameJFrame.java
+++ src/com/blibli/ui/GameJFrame.java
@@ -3,20 +3,36 @@ package com.blibli.ui;
 import javax.swing.*;
 
 public class GameJFrame extends JFrame {
-    public GameJFrame(){
+    public GameJFrame() {
         //初始化界面
         initJFrame();
 
         //初始化菜单
         initJMenuBar();
 
+        //初始化图片
+        initImage();
+
         //显示界面
         this.setVisible(true);
     }
 
-
-
-
+    private void initImage() {
+        //int number = 1;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                //创建一个JLable对象，放置图片的容器
+                JLabel jLabel1 = new JLabel(new ImageIcon());//参数是图片在电脑中的存储路径,此处没有添加
+                //指定第一张图片的位置
+                jLabel1.setBounds(105 * j, 105 * i, 105, 105);
+                //把容器添加到窗口中
+                //this.add(jLabel);
+                this.getContentPane().add(jLabel1);
+                //添加一次图片之后，number自增表示添加下一张图片，图片的命名从1开始自增
+                //number++;
+            }
+        }
+    }
 
 
     private void initJMenuBar() {
@@ -51,17 +67,16 @@ public class GameJFrame extends JFrame {
     }
 
 
-
-
-
     private void initJFrame() {
         //界面的大小
-        this.setSize(600,600);
+        this.setSize(600, 600);
         //设置界面的标题
         this.setTitle("窗口最顶部的显示 游戏1.0");
         //窗口打开直接居中显示
         this.setLocationRelativeTo(null);
         //关闭窗口之后java虚拟机也关闭
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //取消默认居中放置
+        this.setLayout(null);
     }
 }
